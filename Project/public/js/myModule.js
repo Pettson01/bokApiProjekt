d29322842b634bb8cd218cdf3a8014511c6ba95a
Robin Flink <46751884+Pettson01@users.noreply.github.com>
2020-02-18 14:53:44 +0100

    Update myModule.js

--- Project/public/js/myModule.js
+++ Project/public/js/myModule.js
@@ -3,10 +3,6 @@ const path = require('path');
 
 const app = express();
 
-app.use('/static', express.static('public'))
-
-
-
 module.exports = {
     getHomePage: (req, res) => {
       res.render(path.join(__dirname, '../html/index.ejs'),{titel1:"Nu är du inne"});
